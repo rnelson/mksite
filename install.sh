4c8e7ea3ef62a5b10681a24f6570dc085040a974
Ross Nelson <ross.nelson@gmail.com>
2010-08-16 17:31:48 -0500

    Moved the configuration to an example file so I don't have to reset it before committing

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir /etc/mksite
-install -b -c -o root -g wheel -m 0755 mksite.py   /usr/bin/mksite
-install -b -c -o root -g wheel -m 0755 mksite.conf /etc/mksite/mksite.conf
+install -b -c -o root -g wheel -m 0755 mksite.py           /usr/bin/mksite
+install -b -c -o root -g wheel -m 0755 mksite.conf.example /etc/mksite/mksite.conf
 cp -R templates /etc/mksite
 chmod -R 755 /etc/mksite
